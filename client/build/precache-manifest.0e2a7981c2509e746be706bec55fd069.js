e95570724ba3b2f3b524263339f1060a9c948838
TreezCode <TreezCode@users.noreply.github.com>
2019-07-28 23:02:04 -0600

    initial commit

---
+++ client/build/precache-manifest.0e2a7981c2509e746be706bec55fd069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49b6d3f23e2887b1599e",
+    "url": "/static/js/main.6d198c0c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "49b6d3f23e2887b1599e",
+    "url": "/static/css/main.403b92fe.chunk.css"
+  },
+  {
+    "revision": "a8e87442d10479324d640976e694672b",
+    "url": "/index.html"
+  }
+];
