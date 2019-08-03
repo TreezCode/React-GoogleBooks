d82e637191f005131f5b7efbb749374eabaef128
TreezCode <TreezCode@users.noreply.github.com>
2019-08-03 02:16:54 -0600

    app complete

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -6,13 +6,13 @@ import "./Book.css";
 const Book = ({ title, subtitle, authors, link, description, image, Button }) => (
     <ListItem>
         <Row>
-            <Col size="md-9">
+            <Col size="md-8">
                 <h4 className="book-title">{ title }</h4>
                 <h5 className="book-title">{ subtitle }</h5>
                 <p><small>Written by { authors }</small></p>
             </Col>
-            <Col size="md-3">
-                <div className="btn-wrap d-flex">
+            <Col size="md-4">
+                <div className="btn-wrap d-flex mb-2">
                     <a className="btn btn-primary" target="_blank" rel="noopener noreferrer" href={ link }>
                         View
                     </a>
@@ -23,8 +23,8 @@ const Book = ({ title, subtitle, authors, link, description, image, Button }) =>
         <Row>
             <Col size="md-12">
                 <hr className="mt-0"/>
-                <div className="d-flex my-2">   
-                    <img className="thumb pr-3" src={ image } alt={ title } />
+                <div className="my-2">   
+                    <img className="img-fluid thumb pr-3" src={ image } alt={ title } />
                     <p>{ description }</p>
                 </div>
             </Col>
