99965027fffbb6b4c4500eec87403ccc180f0a8a
TreezCode <TreezCode@users.noreply.github.com>
2020-09-11 20:16:32 -0400

    add color to navbar brand

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,7 +6,15 @@ import "./Navbar.css";
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg fixed-top">
         <div className="d-flex">
-            <div className="navbar-brand">GoogleBooks</div>
+            <div className="navbar-brand">
+                <span className="blue-font">G</span>
+                <span className="red-font">o</span>
+                <span className="yellow-font">o</span>
+                <span className="blue-font">g</span>
+                <span className="green-font">l</span>
+                <span className="red-font">e</span>
+                <span>Books</span>
+            </div>
             <NavLink className="nav-link" to="/" activeClassName="active">Search</NavLink>
             <NavLink className="nav-link" to="/Saved" activeClassName="active">Saved</NavLink>
         </div>
